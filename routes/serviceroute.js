45ad0f81d22266978008e1d68219a9a6db666168
Rhea3010 <jain.rhea03@gmail.com>
2022-04-25 19:22:30 +0530

    servicepagedone

--- routes/serviceroute.js
+++ routes/serviceroute.js
@@ -17,6 +17,33 @@ router.get("/ScreenPrinting", (req, res) => {
     res.status(200).render("../views/servicepages/screenoffset.ejs", { tname: "Screen Printing Service" });
 });
 
+router.get("/giftsandpromotion", (req, res) => {
+    res.status(200).render("../views/servicepages/giftsandpromotion.ejs", { tname: "Gifts and Promotion Service" });
+});
+
+router.get("/hoardings", (req, res) => {
+    res.status(200).render("../views/servicepages/hoardings.ejs", { tname: "Hoarding Service" });
+});
+
+router.get("/labelsandposters", (req, res) => {
+    res.status(200).render("../views/servicepages/labelsandposters.ejs", { tname: "Label and Poster Service" });
+});
+
+router.get("/signages", (req, res) => {
+    res.status(200).render("../views/servicepages/signages.ejs", { tname: "Signage Service" });
+});
+
+router.get("/stationaryprinting", (req, res) => {
+    res.status(200).render("../views/servicepages/stationaryprinting.ejs", { tname: "Stationary Printing Service" });
+});
+
+router.get("/visitingcards", (req, res) => {
+    res.status(200).render("../views/servicepages/visitngcards.ejs", { tname: "Visiting Card Service" });
+});
+
+
+
+
 
 
 module.exports = router;
